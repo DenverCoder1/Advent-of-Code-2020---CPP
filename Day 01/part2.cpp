674a71732b814adb6cf93bcee91b843030cc2775
DenverCoder1 <jonah@freshidea.com>
2020-12-02 22:48:23 +0200

    replace tabs with spaces

--- Day 01/part2.cpp
+++ Day 01/part2.cpp
@@ -4,34 +4,34 @@
 using namespace std;
 
 int main() {
-	const int LINES = 200;
-	ifstream inputFile;
-	string line;
-	int input[LINES] = { 0 };
+  const int LINES = 200;
+  ifstream inputFile;
+  string line;
+  int input[LINES] = { 0 };
 
   inputFile.open("input.txt");
 
-	// collect file into array
-	for (int i = 0; i < LINES; ++i) {
-		inputFile >> line;
-		// store as a number
-		input[i] = stoi(line);
-	}
+  // collect file into array
+  for (int i = 0; i < LINES; ++i) {
+    inputFile >> line;
+    // store as a number
+    input[i] = stoi(line);
+  }
 
-	int remaining1, remaining2;
+  int remaining1, remaining2;
 
-	for (int i = 0; i < LINES; ++i) {
-		remaining1 = 2020 - input[i];
-		for (int j = i + 1; j < LINES; ++j) {
-			remaining2 = remaining1 - input[j];
-			for (int k = j + 1; k < LINES; ++k) {
-				if (input[k] == remaining2) {
-					cout << input[i] << "*" << input[j] << "*" << input[k];
-					cout << " = " << (input[i] * input[j] * input[k]);
-				}
-			}
-		}
-	}
+  for (int i = 0; i < LINES; ++i) {
+    remaining1 = 2020 - input[i];
+    for (int j = i + 1; j < LINES; ++j) {
+      remaining2 = remaining1 - input[j];
+      for (int k = j + 1; k < LINES; ++k) {
+        if (input[k] == remaining2) {
+          cout << input[i] << "*" << input[j] << "*" << input[k];
+          cout << " = " << (input[i] * input[j] * input[k]);
+        }
+      }
+    }
+  }
 
   inputFile.close();
   return 0;
